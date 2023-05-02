001de1b3d641c33499777c1171d41c2ca01befd0
John Whitlock <John-Whitlock@ieee.org>
2023-05-02 09:50:41 -0500

    Re-write fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,24 +1,52 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
+  set -gx VISUAL mvim
+  set -gx EDITOR vim
 
-  if test -d /opt/homebrew/bin
-    fish_add_path /opt/homebrew/bin
+  if test -x /opt/homebrew/bin/brew
+    # MacOS, ARM
+    set -gx HOMEBREW_PREFIX "/opt/homebrew"
+  else if test -x /usr/local/bin/brew
+    # MacOS, Intel
+    set -gx HOMEBREW_PREFIX "/usr/local"
+  else
+    set -g --erase HOMEBREW_PREFIX
+  end
 
-    if test -x /opt/homebrew/bin/pyenv
-      pyenv init --path | source
-      pyenv init - | source
+  if test "$HOMEBREW_PREFIX" != ""
+    # Set just HOMEBREW_PREFIX, _CELLAR, etc.
+    eval ($HOMEBREW_PREFIX/bin/brew shellenv | grep -v PATH)
+
+    fish_add_path "$HOMEBREW_PREFIX/sbin"
+    fish_add_path "$HOMEBREW_PREFIX/bin"
+
+    set -q MANPATH; or set MANPATH ''
+    set BREW_MANPATH "$HOMEBREW_PREFIX/share/man"
+    if not contains $BREW_MANPATH $MANPATH
+      set -gx MANPATH $BREW_MANPATH $MANPATH;
+    end
+
+    set -q INFOPATH; or set INFOPATH ''
+    set BREW_INFOPATH "$HOMEBREW_PREFIX/share/info"
+    if not contains $BREW_INFOPATH $INFOPATH
+      set -gx INFOPATH $BREW_INFOPATH $INFOPATH;
+    end
+
+    if test -x "$HOMEBREW_PREFIX/bin/pyenv"
+      "$HOMEBREW_PREFIX/bin/pyenv" init --path | source
+      "$HOMEBREW_PREFIX/bin/pyenv" init - | source
     end
   end
 
-  if test -d /opt/homebrew/sbin
-    fish_add_path /opt/homebrew/sbin
+  if test -x "$HOMEBREW_PREFIX/bin/volta"
+    set -gx VOLTA_HOME "$HOME/.volta"
+    fish_add_path "$VOLTA_HOME/bin"
+  end
+
+  # Google Cloud SDK
+  if test -f "$HOME/google-cloud-sdk/path.fish.inc"
+    . "$HOME/google-cloud-sdk/path.fish.inc"
   end
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/john/google-cloud-sdk/path.fish.inc' ]; . '/Users/john/google-cloud-sdk/path.fish.inc'; end
-
 fish_add_path --append "$HOME/.local/bin"
-
-set -gx VOLTA_HOME "$HOME/.volta"
-fish_add_path "$VOLTA_HOME/bin"
