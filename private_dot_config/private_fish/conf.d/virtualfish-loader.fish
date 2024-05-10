5c9dabe728f042d5c5582617a9abceca12d8f5b9
John Whitlock <jwhitlock@mozilla.com>
2024-05-10 15:28:48 -0500

    Update to virtualfish 2.5.8

--- private_dot_config/private_fish/conf.d/virtualfish-loader.fish
+++ private_dot_config/private_fish/conf.d/virtualfish-loader.fish
@@ -1,9 +1,4 @@
-if test -x /opt/homebrew/Cellar/virtualfish/2.5.7/libexec/bin/vf
-  set -g VIRTUALFISH_VERSION 2.5.7
-  set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/Cellar/virtualfish/2.5.7/libexec/bin/python
-  source /opt/homebrew/Cellar/virtualfish/2.5.7/libexec/lib/python3.12/site-packages/virtualfish/virtual.fish
-  source /opt/homebrew/Cellar/virtualfish/2.5.7/libexec/lib/python3.12/site-packages/virtualfish/compat_aliases.fish
-  source /opt/homebrew/Cellar/virtualfish/2.5.7/libexec/lib/python3.12/site-packages/virtualfish/auto_activation.fish
-  source /opt/homebrew/Cellar/virtualfish/2.5.7/libexec/lib/python3.12/site-packages/virtualfish/projects.fish
-  emit virtualfish_did_setup_plugins
-end
+set -g VIRTUALFISH_VERSION 2.5.8
+set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/Cellar/virtualfish/2.5.8/libexec/bin/python
+source /opt/homebrew/Cellar/virtualfish/2.5.8/libexec/lib/python3.12/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
