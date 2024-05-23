535e02fd053406f5d1d4950268376555978b712a
John Whitlock <jwhitlock@mozilla.com>
2024-05-23 14:46:03 -0500

    Generalize fish loader

--- private_dot_config/private_fish/conf.d/virtualfish-loader.fish
+++ private_dot_config/private_fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,7 @@
-set -g VIRTUALFISH_VERSION 2.5.8
-set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/Cellar/virtualfish/2.5.8/libexec/bin/python
-source /opt/homebrew/Cellar/virtualfish/2.5.8/libexec/lib/python3.12/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
+if set -g VIRTUALFISH_VERSION (/opt/homebrew/bin/brew info --json virtualfish | jq -r ".[0].installed[0].version")
+    set -g VIRTUALFISH_PYTHON_EXEC /opt/homebrew/Cellar/virtualfish/{$VIRTUALFISH_VERSION}/libexec/bin/python
+    source /opt/homebrew/Cellar/virtualfish/{$VIRTUALFISH_VERSION}/libexec/lib/python3.*/site-packages/virtualfish/virtual.fish
+    source /opt/homebrew/Cellar/virtualfish/{$VIRTUALFISH_VERSION}/libexec/lib/python3.*/site-packages/virtualfish/auto_activation.fish
+    source /opt/homebrew/Cellar/virtualfish/{$VIRTUALFISH_VERSION}/libexec/lib/python3.*/site-packages/virtualfish/compat_aliases.fish
+    emit virtualfish_did_setup_plugins
+end
