55d75694a43007971999501096132b9224a33595
John Whitlock <jwhitlock@mozilla.com>
2025-05-06 14:06:24 -0500

    Update deprecated venv-selector config

--- private_dot_config/nvim/lua/plugins_/venv-selector.lua
+++ private_dot_config/nvim/lua/plugins_/venv-selector.lua
@@ -15,29 +15,41 @@ return {
   branch = 'regexp', -- Use regexp branch, as suggested on the main page.
   config = function()
     require('venv-selector').setup {
-      settings = {
-        -- Disable all but the venvs I use (pyenv, virtualenvs). List at:
-        -- https://github.com/linux-cultist/venv-selector.nvim/blob/regexp/lua/venv-selector/config.lua
-        search = {
-          -- The default finds too many pythons
-          virtualenvs = {
-            command = "$FD 'bin/python$' ~/.virtualenvs --full-path --color never",
-          },
-          hatch = false,
-          poetry = false,
-          pipenv = false,
-          anaconda_envs = false,
-          anaconda_base = false,
-          miniconda_envs = false,
-          miniconda_base = false,
-          pipx = false,
-          cwd = false,
-          workspace = false,
-          file = false,
+      -- Disable all but the venvs I use (pyenv, virtualenvs). List at:
+      -- https://github.com/linux-cultist/venv-selector.nvim/blob/regexp/lua/venv-selector/config.lua
+      search = {
+        -- The default finds too many pythons
+        virtualenvs = {
+          command = "$FD 'bin/python$' ~/.virtualenvs --full-path --color never",
         },
-        options = {
-          debug = true,
-        },
+        hatch = false,
+        poetry = false,
+        pipenv = false,
+        anaconda_envs = false,
+        anaconda_base = false,
+        miniconda_envs = false,
+        miniconda_base = false,
+        pipx = false,
+        cwd = false,
+        workspace = false,
+        file = false,
+      },
+      options = {
+        -- Non-defaults
+        debug = true,
+        notify_user_on_venv_activation = true,
+        -- Default values
+        activate_venv_in_terminal = true,
+        cached_venv_automatic_activation = true,
+        enable_default_searches = true,
+        enable_cached_venvs = true,
+        set_environment_variables = true,
+        search_timeout = 5,
+        fd_binary_name = '/opt/homebrew/bin/fd',
+        require_lsp_activation = true,
+        show_telescope_search_type = true,
+        telescope_filter_type = 'substring',
+        telescope_active_venv_color = '#00FF00',
       },
     }
   end,
