7792158e8354f060c4331c235bfd631d85731318
John Whitlock <jwhitlock@mozilla.com>
2025-05-06 14:00:13 -0500

    Disable luarocks
    luarocks uses LuaJIT which uses lua 5.1. Homebrew only has lua 5.4, the
    most recent version. The LuaJIT folks are mad at the Lua folks.

--- private_dot_config/nvim/lua/lazy_.lua
+++ private_dot_config/nvim/lua/lazy_.lua
@@ -26,6 +26,14 @@ require('lazy').setup {
     -- require 'kickstart.plugins.neo-tree',
     -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   },
+  pkg = {
+    sources = {
+      'lazy',
+    },
+  },
+  rocks = {
+    enabled = false,
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
